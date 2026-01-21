36a8542369ff248c8e86f4b99ef485885bc48b1b
guantino <guantino@student.42malaga.com>
2026-01-21 17:29:05 +0100

    norm on instruct_one

--- instruct_one.c
+++ instruct_one.c
@@ -6,16 +6,15 @@
 /*   By: guantino <guantino@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 17:19:14 by guantino          #+#    #+#             */
-/*   Updated: 2026/01/09 14:15:58 by guantino         ###   ########.fr       */
+/*   Updated: 2026/01/21 17:28:19 by guantino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "push_swap.h"
-// (swap first two elements of a)
+
 void	sa(t_stack **a)
 {
-	t_stack *first;
-	t_stack *second;
+	t_stack	*first;
+	t_stack	*second;
 
 	if (!a || !(*a) || !(*a)->next)
 		return ;
@@ -25,11 +24,11 @@ void	sa(t_stack **a)
 	second->next = first;
 	*a = second;
 }
-// (swap first two elements of b)
+
 void	sb(t_stack **b)
 {
-	t_stack *first;
-	t_stack *second;
+	t_stack	*first;
+	t_stack	*second;
 
 	if (!b || !(*b) || !(*b)->next)
 		return ;
@@ -39,17 +38,17 @@ void	sb(t_stack **b)
 	second->next = first;
 	*b = second;
 }
-// (do sa and sb)
+
 void	ss(t_stack **a, t_stack **b)
 {
 	sa(a);
 	sb(b);
 }
-// (take first of b and put it on top of a)
+
 void	pa(t_stack **a, t_stack **b)
 {
 	t_stack	*aux;
-	
+
 	aux = *b;
 	if (!b || !(*b))
 		return ;
@@ -57,11 +56,11 @@ void	pa(t_stack **a, t_stack **b)
 	aux->next = *a;
 	*a = aux;
 }
-// (take first of a and put it on top of b)
+
 void	pb(t_stack **a, t_stack **b)
 {
 	t_stack	*aux;
-	
+
 	aux = *a;
 	if (!a || !(*a))
 		return ;
@@ -69,4 +68,3 @@ void	pb(t_stack **a, t_stack **b)
 	aux->next = *b;
 	*b = aux;
 }
-
