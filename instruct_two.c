ccaa64e0812b3e1959f73700b6d9b4c2297759af
guantino <guantino@student.42malaga.com>
2026-01-21 18:12:20 +0100

    small fixes

--- instruct_two.c
+++ instruct_two.c
@@ -9,20 +9,19 @@
 /*   Updated: 2026/01/09 14:09:30 by guantino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "push_swap.h"
 // (shift up all elements of stack a by 1)
-void	ra(t_stack *a, t_stack *b)
+void	ra(t_stack **a)
 {
 	
 }
 // (shift up all elements of stack b by 1)
-void	rb(t_stack *a, t_stack *b)
+void	rb(t_stack **b)
 {
 
 }
 // (do sa and sb)
-void	rr(t_stack *a, t_stack *b)
+void	rr(t_stack **a, t_stack **b)
 {
 
 }
